186c648341492de2f6b582f861b739b6abb0fffe
Hiki <aabbcdffghtk@gmail.com>
2026-09-02 01:16:10 +0900

    .

--- Assets/Scripts/PlayerScripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerScripts/PlayerBullet.cs
@@ -65,8 +65,9 @@ public class PlayerBullet : Bullet
             if (collision.TryGetComponent(out EnemyBullet eBullet) && eBullet.type == EnemyBullet.bulletType.yellow)
             {
                 Debug.Log("노랑 총알과 충돌");
-                Destroy(gameObject);
                 eBullet.EnemyBulletDamaged(bulletData.damage);
+
+                Destroy(gameObject);
             }
 
             isHit = true;
